6751f56931d47d913c8121dbada3fe218a9574b6
MIDHUN KRISHNA S <krishnasmidhun@gmail.com>
2020-07-05 15:02:45 +0530

    Intial

---
+++ precache-manifest.03f0601fbf2036697d75aed921e02eca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa1d92b0b18d5a47a8d7e696447fdd8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "788f44f94f4f3453b30f",
+    "url": "/static/css/main.9d71b0ad.chunk.css"
+  },
+  {
+    "revision": "46b5848844e4ad6452ea",
+    "url": "/static/js/2.71b15686.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.71b15686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "788f44f94f4f3453b30f",
+    "url": "/static/js/main.e01b40e5.chunk.js"
+  },
+  {
+    "revision": "87649c5cd48c278b84b7",
+    "url": "/static/js/runtime-main.e8675373.js"
+  },
+  {
+    "revision": "7355c6d7147df7d532c0ba3967aeadb9",
+    "url": "/static/media/parallel-parking-1.7355c6d7.jpg"
+  }
+]);
